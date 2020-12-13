ef56df40f3584d37a3aa265933ccc36c846aa7f6
Davi-Mota-Nogueira <davidamota.nogueira@gmail.com>
2020-12-13 02:10:54 -0300

    Começo da 1ª aula de Programação para w

---
+++ programacao-web-javascript/README.md
@@ -0,0 +1,3 @@
+# Programação para internet com JavaScript
+## Atividade 8/24
+Aqui tem a atividade 8 do Decola Dev Avanade, Bootcamp da DIO.
---
+++ programacao-web-javascript/js/main.js
@@ -0,0 +1,12 @@
+var nome = "Davi da Mota Nogueira";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo " + nome + ", você tem " + n1 + " anos.");
+//alert(n1 + n2);
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase);
+console.log(frase.toLowerCase);
