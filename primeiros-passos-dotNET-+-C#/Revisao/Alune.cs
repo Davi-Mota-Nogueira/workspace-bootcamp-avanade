93416cd6a5c95f462169b9c5cb5f4622ba70e026
Davi-Mota-Nogueira <davidamota.nogueira@gmail.com>
2020-12-28 00:51:30 -0300

    Término da aula dos Primeiros Passos com .Net e C#

---
+++ primeiros-passos-dotNET-+-C#/Revisao/Alune.cs
@@ -0,0 +1,9 @@
+namespace Revisao
+{
+    public struct Alune
+    {
+        public string  Nome { get; set; }
+
+        public decimal Nota { get; set; }
+    }
+}
--- primeiros-passos-dotNET-+-C#/Revisao/Program.cs
+++ primeiros-passos-dotNET-+-C#/Revisao/Program.cs
@@ -6,7 +6,107 @@ namespace Revisao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Alune[] alunes = new Alune[5];
+            var contadorAlunes = 0;
+            string opçaoUsuario = OpcaoMenu();
+
+            while (opçaoUsuario.ToUpper() != "X")
+            {
+                switch (opçaoUsuario)
+                {
+                    case "1":
+
+                        Console.WriteLine("Informe o nome de alune: ");
+                        Alune alune = new Alune();
+                        alune.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe agora a nota desse alune: ");
+                        
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            alune.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Coloque a Nota da pessoa vei. Avacalhe não.");
+                        }
+                        
+                        alunes[contadorAlunes] = alune;
+                        contadorAlunes++;
+                        break;
+
+                    case "2":
+
+                        Console.WriteLine("-------------------LISTA DE ALUNOS-------------------\n");
+                        foreach(var a in alunes)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                                Console.WriteLine($"|NOME: {a.Nome} ---- NOTA: {a.Nota}|\n");
+                            
+                        }
+                        break;
+
+
+                    case "3":
+
+                        decimal somaNota = 0;
+                        for(int i = 0; i < alunes.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunes[i].Nome))
+                            {
+                                somaNota += alunes[i].Nota;
+                            }
+                        }
+
+                        var media = (somaNota/contadorAlunes);
+                        ConceitoEnum conceito;
+
+                        if (media >= 8)
+                        {
+                            conceito = ConceitoEnum.A;
+                        }
+                        else if (media < 2)
+                        {
+                            conceito = ConceitoEnum.F;
+                        }
+                        else if (media < 4)
+                        {
+                            conceito = ConceitoEnum.D;
+                        }
+                        else if (media < 6)
+                        {
+                            conceito = ConceitoEnum.C;
+                        }
+                        else
+                        {
+                            conceito = ConceitoEnum.B;
+                        }
+                        
+                        Console.WriteLine($"MEDIA GERAL: {media} ----- CONCEITO: {conceito}");
+                        break;
+
+                    default:
+
+                        throw new ArgumentOutOfRangeException("Escolheu uma opção inexistente. Tente novamente...");
+                }
+                
+                opçaoUsuario = OpcaoMenu();
+            }
+        }
+
+        private static string OpcaoMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Inserir Aluno");
+            Console.WriteLine("2 - Listar Alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opçaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opçaoUsuario;
         }
     }
 }
