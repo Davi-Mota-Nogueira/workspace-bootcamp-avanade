c3e409b785c851ffaa2d0030787ed15adc65178b
Davi-Mota-Nogueira <davidamota.nogueira@gmail.com>
2021-01-02 20:43:39 -0300

    Commit do projeto, só arquivos, porque não funciona fora da pasta do Visual Studio

---
+++ desenvolvimento-aplicacoes-ASP.NET/CursoMVC/CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using CursoAPI.Controllers;
+using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+
+        public CategoriasControllerTest()
+        {
+            _mockSet = new Mock<DbSet<Categoria>>();
+            _mockContext = new Mock<Context>();
+            _categoria = new Categoria { Id = 1, Descricao = "Teste de Categoria" };
+
+            _mockContext.Setup(m => m.Categorias).Returns(_mockSet.Object);
+
+            _mockContext.Setup(m => m.Categorias.FindAsync(1)).ReturnsAsync(_categoria);
+
+            // _mockContext.Setup(m => m.SetModified(_categoria));
+
+            //_mockContext.Setup(m => m.SaveChangesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
+        }
+
+        [Fact]
+        public async Task Get_Categoria()
+        {
+            var service = new CategoriasController(_mockContext.Object);
+
+            await service.GetCategoria(1);
+
+            _mockSet.Verify(m => m.FindAsync(1),
+                Times.Once());
+        }
+
+    }
+}
